2ff5426b539fecc5ac2f63bc78e6e75529b146d9
euernst <euernst@student.ethz.ch>
2021-05-17 20:49:46 +0200

    fixed minor bug in domain adaptation layer

--- Model/DomainAdaptation/domain_adaptation_layer.py
+++ Model/DomainAdaptation/domain_adaptation_layer.py
@@ -192,7 +192,7 @@ class DomainAdaptationLayer(Layer):
         #h = ops.convert_to_tensor(h)
         if self.similarity_measure == 'projected':
             domain_probability = tf.concat([self.calculate_alpha(h, domain) for domain in self.domain_basis.values()], axis=-1)
-            domain_probability = tf.math.divide(domain_probability, reduce_sum(domain_probability, axis=-1, keepdims=True))
+            #domain_probability = tf.math.divide(domain_probability, reduce_sum(domain_probability, axis=-1, keepdims=True))
 
 
         elif self.similarity_measure == 'cosine_similarity':
@@ -442,8 +442,10 @@ class DomainAdaptationLayer(Layer):
 
         elif self.similarity_measure == 'projected':
             domain_probability = tf.concat([self.calculate_alpha(h, domain) for domain in self.domain_basis.values()], axis=-1)
-            domain_probability = tf.math.divide(domain_probability, reduce_sum(domain_probability, axis=-1, keepdims=True))
-            domain_probability = reduce_mean(domain_probability, axis=-1).numpy()
+            #domain_probability = tf.math.divide(domain_probability, reduce_sum(domain_probability, axis=-1, keepdims=True))
+            #domain_probability = reduce_mean(domain_probability, axis=-1)
+
+            domain_probability = reduce_mean(domain_probability, axis=0).numpy()
 
         else:
 
