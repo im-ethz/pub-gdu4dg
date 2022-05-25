fixed training time calculation for FT

SimulationExperiments/digits5/digits_5_classification.py
@@ -235,13 +235,11 @@ def digits_classification(method, TARGET_DOMAIN, single_best=False, single_sourc
     )
 
     run_start = datetime.now()
-    # model.load_weights('my_weights')
     hist = model.fit(x=x_source_tr, y=y_source_tr, epochs=num_epochs, verbose=2,
                      batch_size=batch_size, shuffle=False,
                      validation_data=(x_val, y_val),
                      callbacks=callbacks,
                      )
-    # model.save_weights('my_weights')
     run_end = datetime.now()
 
     # model evaluation
@@ -380,10 +378,12 @@ def digits_classification(method, TARGET_DOMAIN, single_best=False, single_sourc
                 callbacks.append(EarlyStopping(patience=patience, restore_best_weights=True))
 
             print('\n BEGIN FINE TUNING:\t' + similarity_measure.upper() + "\t" + TARGET_DOMAIN[0] + "\n")
+            run_start = datetime.now()
             hist = model.fit(x=x_source_tr, y=y_source_tr.astype(np.float32), epochs=num_epochs_FT, verbose=2,
                              batch_size=batch_size, shuffle=False, validation_data=(x_val, y_val),
                              callbacks=callbacks
                              )
+            run_end = datetime.now()
             model.evaluate(x_target_te, y_target_te, verbose=2)
 
             if save_plot or save_file:

SimulationExperiments/digits5/digits_utils.py
@@ -1,11 +1,12 @@
 import os
+from pathlib import Path
+
 import numpy as np
-from sklearn.metrics import euclidean_distances
 import tensorflow as tf
+import tensorflow_probability as tfp
+from sklearn.metrics import euclidean_distances
 from tensorflow.python.keras.layers import *
 from tensorflow_probability.python.math.psd_kernels.positive_semidefinite_kernel import _SumKernel
-import tensorflow_probability as tfp
-from pathlib import Path
 
 img_shape = (32, 32, 3)
 
@@ -72,12 +73,10 @@ def get_dassl_feature_extractor(dropout=0.5):
         , tf.keras.layers.ReLU()
         , MaxPool2D(pool_size=(2, 2), strides=(2, 2))
 
-	, Conv2D(64, strides=(1, 1), kernel_size=(3, 3), padding="same")
-        , tf.keras.layers.ReLU()
+        , Conv2D(64, strides=(1, 1), kernel_size=(3, 3), padding="same"), tf.keras.layers.ReLU()
         , MaxPool2D(pool_size=(2, 2), strides=(2, 2))
 
-	, Conv2D(64, strides=(1, 1), kernel_size=(3, 3), padding="same")
-        , tf.keras.layers.ReLU()
+        , Conv2D(64, strides=(1, 1), kernel_size=(3, 3), padding="same"), tf.keras.layers.ReLU()
         , MaxPool2D(pool_size=(2, 2), strides=(2, 2))
 
         , Flatten()